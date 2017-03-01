c103394001c4479df34dd272eb531c40877a533e
chinthalakumar <kumarc@newtglobal.com>
2017-03-01 12:46:08 +0530

    Update CustomerServiceApplicationTests.java

--- ECustomer_Microservice-master/ECustomer_Microservice-master/src/test/java/com/example/CustomerServiceApplicationTests.java
+++ ECustomer_Microservice-master/ECustomer_Microservice-master/src/test/java/com/example/CustomerServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+/*package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,4 +17,4 @@ public class CustomerServiceApplicationTests {
 	public void contextLoads() {
 	}
 
-}
+}*/
